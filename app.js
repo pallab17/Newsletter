b62cac5642a1441b3d4887d11a6fafd847d81525
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2023-06-17 00:57:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,59 +1,10 @@
-// const express = require("express")
-// const request = require("request")
-// const bodyParser = require("body-parser")
-// const https = require("https")
-// const mailchimp = require("@mailchimp/mailchimp_marketing");
- 
-// const app = express()
- 
-// mailchimp.setConfig({
-//     apiKey: "a427329642f43dccbf29ee5c2c2f65c4-us11",
-//     server: "us11"
-//   });
- 
-// app.use(express.static("public"))
-// app.use(bodyParser.urlencoded({extended: true}))
- 
-// app.get("/",(req,res) => {
-//     res.sendFile(__dirname + "/signup.html")
-// })
- 
-// app.post("/",function (req,res) {
-//     const listId = "e4e5fe21d9";
-// const subscribingUser = {
-//     firstName: req.body.firstName,
-//     lastName: req.body.lastName,
-//     email: req.body.email
-// };
- 
-// async function run() {
-//     const response = await mailchimp.lists.addListMember(listId, {
-//       email_address: subscribingUser.email,
-//       status: "subscribed",
-//       merge_fields: {
-//         FNAME: subscribingUser.firstName,
-//         LNAME: subscribingUser.lastName
-//       }
-//     });
-  
-//     console.log(
-//       `Successfully added contact as an audience member. The contact's id is ${response.id}.`
-//     );
-//   }
- 
-// run();
- 
- 
-// });
- 
-// app.listen(3000, () => {
-//     console.log("Server is running on port 3000");
-// })
+
 
 // import required packages
 const express= require("express");
 const https= require("https");
 const bodyparser= require("body-parser");
+require('dotenv').config()
 
 const app= express();
 app.use(express.static("public"));
@@ -85,10 +36,10 @@ var data={
 
 // Converting string data to JSON data
 const jsonData= JSON.stringify(data);
-const url="https://us11.api.mailchimp.com/3.0/lists/e4e5fe21d9";
+const url="https://us11.api.mailchimp.com/3.0/lists/process.env.audience_id";
 const options={
 method:"POST",
-auth:"pallab:a427329642f43dccbf29ee5c2c2f65c4-us11"
+auth:process.env.API_KEY
 }
 
 // On success send users to success, otherwise on failure template
