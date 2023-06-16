2396b06290095393669ac89f276aeab63e166350
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2023-06-17 04:09:02 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,67 +1,87 @@
 
-
-// import required packages
-const express= require("express");
-const https= require("https");
-const bodyparser= require("body-parser");
-require('dotenv').config()
-
-const app= express();
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 3000;
+// const request = require("request");
+const https = require('https');
+// const API_KEY = process.env.API_KEY;
+const audianceID = "e4e5fe21d9"; 
+ 
+ 
 app.use(express.static("public"));
-
-app.use(bodyparser.urlencoded({extended:true}));
-
-// On the home route, send signup html template
-app.get("/",function(req,res){
-res.sendFile(__dirname+"/signin.html");
-});
-
-// Manage post request on home route and
-// Send data to the MailChimp account via API
-app.post("/",function(req,res){
-var firstName=req.body.fname;
-var lastName = req.body.lName;
-var email=req.body.email;
-
-var data={
-	members:[{
-	email_address: email,
-	status: "subscribed",
-	merge_fields:{
-		FNAME: firstName,
-		LNAME: lastName
-	}
-	}]
-}
-
-// Converting string data to JSON data
-const jsonData= JSON.stringify(data);
-const url="https://us11.api.mailchimp.com/3.0/lists/process.env.audience_id";
-const options={
-method:"POST",
-auth:process.env.API_KEY
-}
-
-// On success send users to success, otherwise on failure template
-const request=https.request(url,options,function(response){
-if(response.statusCode===200){
-	res.sendFile(__dirname+"/success.html");
-}else{
-	res.sendFile(__dirname+"/failure.html");
-}
-response.on("data",function(data){
-	console.log(JSON.parse(data));
-});
-});
-request.write(jsonData);
-request.end();
-});
-
-// Failure route
-app.post("/failure",function(req,res){
-res.redirect("/");
+ 
+app.listen(PORT || process.env.PORT, (req, res) => {
+    console.log("App is listening on Port : ", PORT);
 })
-
-app.listen(process.env.PORT || 8000,function(){
-console.log("server is running on port 8000.");
+ 
+//redirect to signup page
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signin.html");
 })
+ 
+//setting bodyparser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+//signup route setting up post method and connecting to the mailchimp server
+app.post('/', (req, res) => {
+ 
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+ 
+    //setting up data for mailchimp endpoint
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    };
+ 
+ 
+    const jsonData = JSON.stringify(data);
+ 
+    const url = `https://us11.api.mailchimp.com/3.0/lists/${audianceID}`;
+    const options = {
+ 
+        method: "POST",
+        headers: {
+            Authorization: `auth ${process.env.API_KEY}`
+        },
+ 
+    }
+ 
+    //setting up response for success & failure
+    const request = https.request(url, options, (response) => {
+        if (response.statusCode === 200) {
+            // res.send("success")
+             res.sendFile(__dirname + "/success.html");
+        } else {
+             res.sendFile(__dirname + "/failure.html");
+            // res.send("success")
+        }
+ 
+        // getting data 
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        });
+    });
+ 
+    // sinmitting data to mailchimp
+    request.write(jsonData);
+    request.end();
+ 
+});
+ 
+//redirect to the main page if failed
+app.post("/failure", (req, res) => {
+    res.redirect("/");
+})
+ 
